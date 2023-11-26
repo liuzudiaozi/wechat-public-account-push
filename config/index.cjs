5468786bfa22ac349d6d8a8733abc3bbbc4bbc36
liuzudiaozi <152019670+liuzudiaozi@users.noreply.github.com>
2023-11-26 12:13:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od9Q16e92Fsz0V7SUSQm5m-SNNP8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'trXRjF357Khqb2UN8tqlTyHTD-XRH0xiIutvLN8xRXc',
+      useTemplateId: 'IbPu1KvF7BJlOXNbHx73gyjL8i_6ORCf893BUwTLWPI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'trXRjF357Khqb2UN8tqlTyHTD-XRH0xiIutvLN8xRXc',
+  CALLBACK_TEMPLATE_ID: 'IbPu1KvF7BJlOXNbHx73gyjL8i_6ORCf893BUwTLWPI',
 
   CALLBACK_USERS: [
     {
